525943fa386bcade220f2d93e6b66963aa5074a8
Davide Merli <davide.merli@outlook.com>
2021-06-28 18:41:03 +0200

    feat: better polaroid with background

--- lib/widgets/polaroid.dart
+++ lib/widgets/polaroid.dart
@@ -19,12 +19,14 @@ class _StepCurve extends Curve {
 class Polaroid extends ConsumerWidget {
   Polaroid({
     Key? key,
-    required this.duration,
-    required this.target,
+    required this.targetLabel,
+    required this.targetPicture,
+    required this.animateDuration,
   }) : super(key: key);
 
-  final Duration duration;
-  final String target;
+  final String targetLabel;
+  final Widget targetPicture;
+  final Duration animateDuration;
 
   @override
   Widget build(BuildContext context, ScopedReader watch) {
@@ -36,32 +38,36 @@ class Polaroid extends ConsumerWidget {
       child: AspectRatio(
         aspectRatio: 3.5 / 4.2,
         child: AnimatedContainer(
-          padding: EdgeInsets.all(rotationState ? 10 : 0),
-          duration: duration,
+          duration: animateDuration,
           transformAlignment: Alignment.center,
           transform: Matrix4.identity()
             ..rotateY(rotationState ? 0 : pi)
             ..rotateZ(rng.nextDouble() * pi / 8),
           curve: Curves.easeInOut,
-          color: Colors.blue,
-          child: AnimatedOpacity(
-            curve: _StepCurve(),
-            duration: duration,
-            opacity: rotationState ? 1 : 0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                AspectRatio(
-                  aspectRatio: 1,
-                  child: Container(
-                    transform: Matrix4.identity()..translate(0.0, 0.0, 10.0),
-                    child: Image.asset("assets/logo.jpg"),
+          child: Stack(
+            children: [
+              Image(image: AssetImage("assets/polaroid.png")),
+              AnimatedOpacity(
+                curve: _StepCurve(),
+                duration: animateDuration,
+                opacity: rotationState ? 1 : 0,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      AspectRatio(aspectRatio: 1, child: targetPicture),
+                      Text(
+                        targetLabel,
+                        //TODO: add 'handwritten' font
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      SizedBox(height: 50)
+                    ],
                   ),
                 ),
-                Text(target, style: Theme.of(context).textTheme.headline4),
-                SizedBox(height: 4)
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
