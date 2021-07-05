94e1fe0e01c40825f7ec18ed86991ad1cd2eb6da
Davide Merli <davide.merli@outlook.com>
2021-07-05 10:58:58 +0200

    feat: add new design elements

---
+++ lib/colors.dart
@@ -0,0 +1,8 @@
+// Flutter imports:
+import 'package:flutter/cupertino.dart';
+
+const Color assassinRed = Color(0xFFC33C54);
+const Color assassinDarkestBlue = Color(0xFF254E70);
+const Color assassinDarkBlue = Color(0xFF37718E);
+const Color assassinBlue = Color(0xFF8EE3EF);
+const Color assassinLightBlue = Color(0xFFAEF3E7);
