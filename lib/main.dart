ead29fb3664af762d178d6a0c89fc97ba2049f40
Davide Merli <davide.merli@outlook.com>
2021-07-19 00:07:31 +0200

    feat: add font initialization

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:assassin_client/pages/edit_profile.dart';
 import 'package:assassin_client/pages/logged_in.dart';
 import 'package:assassin_client/pages/login.dart';
+import 'package:assassin_client/pages/register.dart';
 import 'package:assassin_client/pages/target.dart';
 
 void main() {
@@ -19,6 +20,7 @@ void main() {
 
 final routes = {
   '/login': (context) => LoginRoute(),
+  '/register': (context) => RegisterRoute(),
   '/homepage': (context) => HomePage(),
   '/target': (context) => const TargetRoute(),
   '/edit-profile': (context) => const EditProfileRoute(),
@@ -42,9 +44,7 @@ class AssassinApp extends ConsumerWidget {
 
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: _buildTheme(),
       debugShowCheckedModeBanner: false,
       home: firebase.when(
         loading: () => Center(child: CircularProgressIndicator()),
@@ -52,6 +52,21 @@ class AssassinApp extends ConsumerWidget {
         data: (firebase) => LoginRoute(),
       ),
       routes: routes,
+    );
+  }
+
+  ThemeData _buildTheme() {
+    return ThemeData(
+      primarySwatch: Colors.blue,
+      fontFamily: 'Open Sans',
+      textTheme: TextTheme(
+        headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+        headline6: TextStyle(fontSize: 36.0, fontWeight: FontWeight.w800),
+        bodyText1: TextStyle(fontSize: 18.0),
+        bodyText2: TextStyle(fontSize: 14.0),
+        button: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w900),
+        headline5: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w900),
+      ),
     );
   }
 }
