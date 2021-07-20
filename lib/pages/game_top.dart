c44d9fb31c19628693f23cc3b61494ff6f763f62
Dario Passarello <dario.passarello@gmail.com>
2021-07-20 09:40:48 +0200

    feat: game scrolling view

--- lib/pages/game_top.dart
+++ lib/pages/game_top.dart
@@ -1,18 +1,38 @@
+// Dart imports:
+import 'dart:async';
+
 // Flutter imports:
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+// Package imports:
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 // Project imports:
 import 'package:assassin_client/colors.dart';
 
-class GameRoute extends StatelessWidget {
-  //final _pageViewController = PageController(initialPage: 1);
+class GameRoute extends ConsumerWidget {
+  /// Used to trigger an event when the widget has been built
+  Future<bool> _afterWidgetBuilt() {
+    final completer = Completer<bool>();
+    // Register callback after the frame is built
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      completer.complete(true);
+    });
+    return completer.future;
+  }
+
+  final pageControllerProvider =
+      ChangeNotifierProvider((_) => PageController(initialPage: 0));
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader watch) {
+    final pageController = watch(pageControllerProvider);
+
     return Scaffold(
       backgroundColor: assassinDarkestBlue,
       body: PageView(
+        controller: pageController,
         children: [
           Container(color: Colors.red),
           Container(color: Colors.green),
@@ -22,18 +42,66 @@ class GameRoute extends StatelessWidget {
       appBar: AppBar(
         title: Text('PARTITA DEMOCRATICA'),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.ac_unit),
-              activeIcon: Icon(Icons.safety_divider),
-              label: 'Perimetro'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.nat),
-              activeIcon: Icon(Icons.water_damage),
-              label: 'Cavolfiore'),
-        ],
-      ),
+      bottomNavigationBar: FutureBuilder(
+          future: _afterWidgetBuilt(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return Container();
+            return BottomNavigationBar(
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              currentIndex:
+                  pageController.page?.round() ?? pageController.initialPage,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.center_focus_strong),
+                  activeIcon: SizedBox(
+                    height: 24.0,
+                    width: 24.0,
+                    child: CustomPaint(
+                      painter: const FingerPainter(Colors.red),
+                    ),
+                  ),
+                  label: 'target',
+                ),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.perm_identity),
+                    activeIcon: Icon(Icons.water_damage),
+                    label: 'id'),
+              ],
+              onTap: (page) => pageController.jumpToPage(page),
+            );
+          }),
     );
+  }
+}
+
+class FingerPainter extends CustomPainter {
+  final Color color;
+
+  const FingerPainter(this.color);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    //Valid values for Xs -> (0, 0.5)
+    final ctrl1X = .20;
+    final ctrl1Y = .10;
+    final ctrl2X = .10;
+    final paint = Paint()
+      ..color = color
+      ..style = PaintingStyle.fill;
+    // https://cubic-bezier.com/#.40,.20,.20,1.00
+    final path = Path()
+      ..moveTo(0, size.height)
+      ..cubicTo(ctrl1X * size.width, (1 - ctrl1Y) * size.height,
+          ctrl2X * size.width, 0, size.width * .5, 0)
+      ..cubicTo((1 - ctrl2X) * size.width, 0, (1 - ctrl1X) * size.width,
+          (1 - ctrl1Y) * size.height, size.width, size.height);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant FingerPainter oldDelegate) {
+    return oldDelegate.color != color;
   }
 }
